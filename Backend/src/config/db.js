c2e7e729b4e91eac61036cf6469f0064453a1143
abshar-hspl <Abshar.Fatima@harbingergroup.com>
2025-02-04 16:05:34 +0530

    Added mongoDB atlas connection

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" }); 
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI1 = process.env.MONGO_URI1;
 
-if (!MONGO_URI) {
+if (!MONGO_URI1) {
   console.error("MONGO_URI is not defined. Check your .env file.");
   process.exit(1);
 }
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI, {
+    const conn = await mongoose.connect(MONGO_URI1, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
